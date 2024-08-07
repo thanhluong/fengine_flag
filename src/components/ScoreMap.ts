765bbf648122eb39061ff897ef04c9539357e40f
thanhluong <doannguyenthanhluong@gmail.com>
2024-08-08 03:35:43 +0700

    Done map 7

--- src/components/ScoreMap.ts
+++ src/components/ScoreMap.ts
@@ -11,7 +11,7 @@ export default class ScoreMap {
 
   scoreArray: number[][][];
   scoreArrayInstance: ScoreArray;
-  idArray: number = 6;
+  idArray: number = 7;
   defaultRed: number = 255;
   defaultGreen: number = 255;
   defaultBlue: number = 103;
