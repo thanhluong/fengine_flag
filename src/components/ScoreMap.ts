47c431ce66d8a3a689ca252e4c835c6e057ff06e
thanhluong <doannguyenthanhluong@gmail.com>
2024-08-08 00:42:28 +0700

    Set first element to zero

--- src/components/ScoreMap.ts
+++ src/components/ScoreMap.ts
@@ -16,7 +16,7 @@ export default class ScoreMap {
   defaultGreen: number = 255;
   defaultBlue: number = 103;
   fiboNumber: number[] = [
-    30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45,
+    0, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45,
   ];
   constructor() {
     this.scores = [];
