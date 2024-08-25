1a924338ffc6fa0e7a169e39458046a639c53ed4
thanhluong <doannguyenthanhluong@gmail.com>
2024-08-25 15:19:04 +0700

    Update latest version

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -9,7 +9,7 @@ import ScoreMap from "../../components/ScoreMap.ts";
 import axios from "axios";
 // import {c} from "vite/dist/node/types.d-aGj9QkWt";
 
-const mapIndex = 6;
+const mapIndex = 10;
 const updateDelay = 3000;
 const moveDelay = 150;
 
@@ -105,7 +105,7 @@ export class Game extends Scene {
     this.load.setPath("assets");
     this.load.tilemapTiledJSON(
       "tilemap",
-      `Tilemap/tilemap16x16ver${mapIndex}.json`
+      `Tilemap/tilemap16x16ver${mapIndex}.json`,
     );
     // this.load.tilemapTiledJSON("tilemap", `Tilemap/tilemap.json`);
     this.load.image("grass-ts", "Tilemap/Grass.png");
@@ -190,7 +190,7 @@ export class Game extends Scene {
     this.player1 = this.physics.add.sprite(
       tileSz * endPoint[1] + tileSz * 0.5,
       tileSz * endPoint[1] + tileSz * 0.5,
-      "cute1"
+      "cute1",
     );
     this.player1
       .setBodySize(this.player1.width / 3, this.player1.height / 3)
@@ -201,7 +201,7 @@ export class Game extends Scene {
     this.player2 = this.physics.add.sprite(
       tileSz * endPoint[1] + tileSz * 0.5,
       tileSz * endPoint[1] + tileSz * 0.5,
-      "cute2"
+      "cute2",
     );
     this.player2
       .setBodySize(this.player2.width / 3, this.player2.height / 3)
@@ -220,7 +220,7 @@ export class Game extends Scene {
       this.bombs2,
       this.player2,
       "bomb",
-      this.scoreMap
+      this.scoreMap,
     );
     this.components.addComponent(this.player1, this.inputBomb);
     this.inputComponent = new InputComponent(this.fences, this.player1);
@@ -229,11 +229,11 @@ export class Game extends Scene {
     // Add player2 components
     this.components.addComponent(
       this.player2,
-      new KeyboardMovement(this.cursors)
+      new KeyboardMovement(this.cursors),
     );
     this.components.addComponent(
       this.player2,
-      new KeyboardMovement(this.cursors)
+      new KeyboardMovement(this.cursors),
     );
     this.components.addComponent(this.player2, new KeyboardAnimation("cute2"));
     this.inputBomb2 = new InputBomb(
@@ -241,7 +241,7 @@ export class Game extends Scene {
       this.bombs,
       this.player1,
       "flag-blue",
-      this.scoreMap
+      this.scoreMap,
     );
     this.components.addComponent(this.player2, this.inputBomb2);
     this.inputComponent2 = new InputComponent(this.fences, this.player2);
@@ -375,7 +375,7 @@ export class Game extends Scene {
   renderBorder(layer: Phaser.Tilemaps.TilemapLayer) {
     const debugGraphics = this.add.graphics().setAlpha(0.5);
     debugGraphics.lineStyle(2, 0xffffff, 0.25);
-    layer.forEachTile(tile => {
+    layer.forEachTile((tile) => {
       if (tile.index !== -1) {
         const x = tile.getLeft();
         const y = tile.getTop();
@@ -422,7 +422,7 @@ export class Game extends Scene {
         this.player2,
         this.beginPosition1,
         this.beginPosition2,
-        1
+        1,
       );
       this.inputForB = this.renderPostition(
         blockSize,
@@ -432,7 +432,7 @@ export class Game extends Scene {
         this.player1,
         this.beginPosition2,
         this.beginPosition1,
-        2
+        2,
       );
     }
 
@@ -468,7 +468,7 @@ export class Game extends Scene {
       binary: string,
       id: number,
       input: string,
-      typeLanguage: string
+      typeLanguage: string,
     ) => {
       const response = await axios.post(`${EXECUTOR_URL}/run_code`, {
         code: binary,
@@ -483,13 +483,13 @@ export class Game extends Scene {
       localStorage.getItem("binaryCodeA")!,
       1,
       this.inputForA,
-      localStorage.getItem("languageA")!
+      localStorage.getItem("languageA")!,
     );
     await getOutput(
       localStorage.getItem("binaryCodeB")!,
       2,
       this.inputForB,
-      localStorage.getItem("languageB")!
+      localStorage.getItem("languageB")!,
     );
   }
   async updateKmove(dt: number, k: number) {
@@ -502,10 +502,10 @@ export class Game extends Scene {
     this.inputComponent.update();
     await this.inputComponent2.update();
     this.textP1.setText(
-      `\nScore:${this.scoreMap.getScore(1)} \nMove:${this.output[1]?.charAt(k) ?? ""}`
+      `\nScore:${this.scoreMap.getScore(1)} \nMove:${this.output[1]?.charAt(k) ?? ""}`,
     );
     this.textP2.setText(
-      `\nScore:${this.scoreMap.getScore(2)}\nMove:${this.output[2]?.charAt(k) ?? ""}`
+      `\nScore:${this.scoreMap.getScore(2)}\nMove:${this.output[2]?.charAt(k) ?? ""}`,
     );
     console.log("Done move");
   }
@@ -517,7 +517,7 @@ export class Game extends Scene {
       startX > 0 && startX < tileSz && startY > 0 && startY < tileSz;
     const isValid = this.fences.getTileAtWorldXY(
       this.tileToPixel(startX),
-      this.tileToPixel(startY)
+      this.tileToPixel(startY),
     );
     if (isValid != null || !isInMap) {
       startX = 2;
@@ -540,7 +540,7 @@ export class Game extends Scene {
   renderMapScoreAndState(
     startPoint: [number, number],
     endPoint: [number, number],
-    scoreMap: number[][]
+    scoreMap: number[][],
   ) {
     for (let i = startPoint[0]; i <= endPoint[0]; i++) {
       for (let j = startPoint[1]; j <= endPoint[1]; j++) {
@@ -593,7 +593,7 @@ export class Game extends Scene {
     player2: Phaser.Physics.Arcade.Sprite,
     beginPosition1: number[],
     beginPosition2: number[],
-    id: number
+    id: number,
   ) {
     let playerInput = input;
     playerInput +=
